df07c423e82faf58a90f3e82a81485bd68918550
Cole Gulledge <colegulledge@Coles-MacBook-Air.local>
2023-12-26 16:20:59 -0500

    [secrets[: adding to secret and such

--- main.py
+++ main.py
@@ -1,52 +1,20 @@
 # importing the libraries 
-from bs4 import BeautifulSoup 
-import requests 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.cloud import secretmanager
 
-# creating function 
-def scrape_info(url): 
-	
-	# getting the request from url 
-	r = requests.get(url) 
-	
-	# converting the text 
-	s = BeautifulSoup(r.text, "html.parser") 
-	
-	# finding meta info for title 
-	title = s.find("span", class_="watch-title").text.replace("\n", "") 
-	
-	# finding meta info for views 
-	views = s.find("div", class_="watch-view-count").text 
-	
-	# finding meta info for likes 
-	likes = s.find("span", class_="like-button-renderer").span.button.text 
-	
-	# saving this data in dictionary 
-	data = {'title':title, 'views':views, 'likes':likes} 
-	
-	# returning the dictionary 
-	return data 
-
-# main function 
-# if __name__ == "__main__": 
-	
-# 	# URL of the video 
-# 	url ="https://www.youtube.com/watch?time_continue=17&v=2wEA8nuThj8"
-	
-# 	# calling the function 
-# 	data = scrape_info(url) 
-	
-# 	# printing the dictionary 
-# 	print(data) 
-
-
-#sGET https://www.googleapis.com/youtube/v3/search?key={your_key_here}&channelId={channel_id_here}&part=snippet,id&order=date&maxResults=20
-
-
-DEVELOPER_KEY = 'AIzaSyBojdanEe9sdgK1hdspp2pYaHoupzOYRFU'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+
+def get_key(): 
+    client = secretmanager.SecretManagerServiceClient()
+    name = "projects/639888050178/secrets/youtube-key/versions/1"
+    # Get the secret version.
+    response = client.access_secret_version(request={"name": name})
+    # Print information about the secret version.
+    payload = response.payload.data.decode("UTF-8")
+    print(payload)
 
 def youtube_search():
     videos = []
@@ -56,7 +24,7 @@ def youtube_search():
         part="contentDetails",
         id="UCy64jj2RTOM8lILQNGHF02A"
         ).execute()
-    print(response)
+    #print(response)
     playlist_id = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
     # Retrieve videos from the playlist
@@ -80,10 +48,20 @@ def youtube_search():
             break
 
     return videos
-vids = youtube_search()
-for video in vids:
-        title = video["snippet"]["title"]
-        views = video["statistics"]["viewCount"]
-        print(f"Video Title: {title}")
-        print(f"Views: {views}")
+if __name__ == "__main__": 
+    videos = youtube_search()
+    secret = get_key()
+    for vid in videos:
+        title = vid["snippet"]["title"]
+        published = vid["snippet"]["publishedAt"]
+        id = vid["snippet"]["resourceId"]["videoId"]
+        vids = youtube.videos().list(
+            part = "statistics",
+            id = id).execute()
+        views = (vids['items'][0]['statistics']['viewCount'] )
+        print(f"{title} with the id: {id} has {views} views" )
+
+    
+    
+    
         
