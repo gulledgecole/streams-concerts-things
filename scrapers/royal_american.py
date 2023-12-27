bda3be494708b51d05deb7790a75f8559d1dfded
Cole Gulledge <colegulledge@Coles-MacBook-Air.local>
2023-12-27 17:43:25 -0500

    [folder] creating folder, concating the dicts, adding royal constants

--- scrapers/royal_american.py
+++ scrapers/royal_american.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from dateutil import parser
+import json
 import requests
 import re
 
@@ -19,6 +20,8 @@ def scrape_royal(url):
         html_content = data.text
         soup = BeautifulSoup(html_content, 'html.parser')
         div_elements = soup.find_all('div', class_='eventlist-column-info')
+        data = {}
+        iteration_counter = 1
         if div_elements:
             for div_element in div_elements:
                 title = div_element.find('h1', class_='eventlist-title').text.strip()
@@ -28,11 +31,21 @@ def scrape_royal(url):
                 cleaned_band_list1 = [a.get_text(strip=True)[2:] for a in input_string.find_all('a') if a.get_text(strip=True)]
                 cleaned_band_list = [band for band in cleaned_band_list1 if band]
                 event_json = {
+                    "Venue" : "The Royal American",
+                    "street" : "970 Morrison Dr", 
+                    "City" : "Charleston",
+                    "State" : "South Carolina",
+                    "Long" : "-79.942380", 
+                    "Lat" : "32.807180",
                     "Title": title,
                     "Date": date,
                     "Bands" : cleaned_band_list
                 }
+                data[iteration_counter] = event_json
+                iteration_counter += 1
         else:
             print("No div elements with the specified class found.")
+        
+        return data
 if __name__ == '__main__':
-    scrape_royal('https://www.theroyalamerican.com/schedule/')
+    event = scrape_royal('https://www.theroyalamerican.com/schedule/')
