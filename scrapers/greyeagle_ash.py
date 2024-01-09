ffbf6a0723f19fc8020e6586189006577a3a30c1
Cole Gulledge <gulledgecole1@gmail.com>
2024-01-09 17:05:41 -0500

    [scraping]: beating the forbidden with random

--- scrapers/greyeagle_ash.py
+++ scrapers/greyeagle_ash.py
@@ -3,11 +3,16 @@ from dateutil import parser
 import date_shift
 import json
 import requests
+import random
 import re
 
 def scrape_greyeagleash(url):
-    headers = {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-    data = requests.get(url)
+    user_agents_list = [
+    'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+    ]
+    data = requests.get(url, headers={'User-Agent': random.choice(user_agents_list)})
     print(data)
     if data.status_code == 200:
         html_content = data.text
