2e551c5d8b2d56b0e1aada5d7dd71466aafb8805
Cole Gulledge <gulledgecole1@gmail.com>
2024-01-20 11:19:38 -0500

    [counter] unique counter script

--- scrapers/counter.py
+++ scrapers/counter.py
@@ -4,10 +4,10 @@ import csv
 
 # Extract x-jamBaseMetroId values
 
-with open('us_dump_100k.json', 'r') as file:
+with open("us_dump_100k.json", "r") as file:
     data = json.load(file)
 
-metro_ids = [entry.get('address', {}).get('x-jamBaseMetroId') for entry in data]
+metro_ids = [entry.get("address", {}).get("x-jamBaseMetroId") for entry in data]
 
 # Filter out None values
 metro_ids = [id for id in metro_ids if id is not None]
@@ -23,10 +23,10 @@ max_count = max(metro_id_counts.values())
 print("Unique x-jamBaseMetroId values:", unique_metro_ids)
 print("Maximum occurrence count:", max_count)
 
-csv_filename = 'metro_id_counts.csv'
-with open(csv_filename, 'w', newline='') as csvfile:
+csv_filename = "metro_id_counts.csv"
+with open(csv_filename, "w", newline="") as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['x-jamBaseMetroId', 'Count'])
+    csv_writer.writerow(["x-jamBaseMetroId", "Count"])
 
     for metro_id, count in metro_id_counts.items():
         csv_writer.writerow([metro_id, count])
